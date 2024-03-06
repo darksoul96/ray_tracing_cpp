ace23a3240f6785db843c08b3ca59eb5703b0bf7
Agustin Ruiz <agustinruiz96@gmail.com>
2024-03-06 13:05:43 -0300

    basic image + cmake file + build output files

---
+++ build/CMakeFiles/RayTracing_by_Agustin_Ruiz.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracing_by_Agustin_Ruiz.dir/main.cpp.o"
+  "CMakeFiles/RayTracing_by_Agustin_Ruiz.dir/main.cpp.o.d"
+  "RayTracing_by_Agustin_Ruiz"
+  "RayTracing_by_Agustin_Ruiz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracing_by_Agustin_Ruiz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/RayTracing_by_Agustin_Ruiz.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RayTracing_by_Agustin_Ruiz.
+# This may be replaced when dependencies are built.
